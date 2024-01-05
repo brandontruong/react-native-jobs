437832f93a526ab99320b1fd8b48474600d18caf
brandontruong <brandon.truong@outlook.com>
2024-01-05 18:33:53 +1100

    initial set up

---
+++ app/index.js
@@ -0,0 +1,11 @@
+import { View, Text } from 'react-native';
+
+const Home = () => {
+    return (
+        <View>
+            <Text>Home is here OOMG bsndon tripng</Text>
+        </View>
+    )
+}
+
+export default Home;
